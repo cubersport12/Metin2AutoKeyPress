8bb2987d9fa8ad49159ef5acb8eefb0af642384b
cubersport123455 <cubersport123455@andex.ru>
2024-06-10 22:28:27 +0600

    tree

---
+++ Application/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<ViewItem> ListBoxSource { get; set; }
+        public ObservableCollection<ViewItem> TreeViewSource { get; set; }
+
+        public MainViewModel()
+        {
+            ListBoxSource = new ObservableCollection<ViewItem>();
+            TreeViewSource = new ObservableCollection<ViewItem>();
+
+            for (int i = 1; i < 20; i++) ListBoxSource.Add(new ViewItem($"ListBoxItem {i}"));
+
+            ViewItem defaultView = new ViewItem("Root", 1);
+            TreeViewSource.Add(defaultView);
+            for (int i = 1; i < 10; i++) defaultView.ViewItems.Add(new ViewItem($"TreeViewItem {i}"));
+        }
+    }
+}
---
+++ Application/ViewItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class ViewItem
+    {
+        public string ItemName { get; set; }
+        public ObservableCollection<ViewItem> ViewItems { get; } = new ObservableCollection<ViewItem>();
+        public int IsVisible { get; set; }
+
+        public ViewItem()
+        { }
+
+        public ViewItem(string name, int IsVisible = 0)
+        {
+            this.ItemName = name;
+            this.IsVisible = IsVisible;
+        }
+    }
+}
